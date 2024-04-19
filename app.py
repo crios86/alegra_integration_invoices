9c7afe5c8553186b1e3c807d1f63a8ca1143d838
crios86 <criosriosm86@gmail.com>
2024-04-19 13:54:37 -0500

    version 1.2 invoice_send.py add

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from customer_update import fetch_all_contacts, fetch_customer_names,visualizar_clientes
 from items_update import fetch_all_items, create_dataframe, create_sqlite_database,visualizar_productos
+from invoice_send import invoces_send_alegra
 import pandas as pd
-from datetime import datetime
-import requests
 
 
 app = Flask(__name__)
@@ -26,7 +25,6 @@ def get_product_names():
 @app.route('/procesar_facturas', methods=['POST'])
 def procesar_facturas():
     try:
-
         # Obtener los datos del formulario
         razones_sociales = request.form.getlist('razon_social')
         nombres_productos = request.form.getlist('nombre_producto')
@@ -48,95 +46,7 @@ def procesar_facturas():
         df = pd.DataFrame(data)
         print(df)
 
-        clientes = visualizar_clientes().drop_duplicates()
-        nuevos_nombres = {'ID': 'codigo_cliente', 'Name': 'nombre_cliente'}
-        clientes = clientes.rename(columns=nuevos_nombres)
-        productos = visualizar_productos().drop_duplicates()
-        nuevos_nombres = {'ID': 'codigo_producto', 'Name': 'nombre', 'UnitCost':'precio_unitario'}
-        productos = productos.rename(columns=nuevos_nombres)
-        pedidos_df = df.copy()
-
-        cod_cliente = []
-        for razon_social in list(pedidos_df['razon_social']):
-            resultado_loc = clientes.loc[clientes['nombre_cliente'] == razon_social]
-            cod_cliente.append(resultado_loc['codigo_cliente'].iloc[0])
-
-        cod_producto = []
-        for nombre_producto in list(pedidos_df['nombre_producto']):
-            resultado_loc = productos.loc[productos['nombre'] == nombre_producto]
-            cod_producto.append(resultado_loc['codigo_producto'].iloc[0])
-
-        unit_price = []
-        for nombre_producto in list(pedidos_df['nombre_producto']):
-            resultado_loc = productos.loc[productos['nombre'] == nombre_producto]
-            unit_price.append(resultado_loc['precio_unitario'].iloc[0])
-
-        df_codigos = {
-                        'codigo_cliente' :cod_cliente,
-                        'codigo_producto' : cod_producto,
-                        'precio_unitario' :unit_price,
-                        'fecha_creacion'  : datetime.now().strftime('%Y-%m-%d')
-                    }
-        
-        df_codigos = pd.DataFrame(df_codigos)
-        pedidos_df = pd.concat([pedidos_df, df_codigos], axis=1)
-
-        # Eliminar filas con valores NaN
-        pedidos_df.dropna(inplace=True)
-
-        # Conversión de tipos de datos
-        pedidos_df = pedidos_df.dropna().astype({'codigo_cliente': int, 'codigo_producto': int, 'cantidad': int, 'precio_unitario': int})
-
-        for codigo_cliente in pedidos_df['codigo_cliente'].unique():
-            df = pedidos_df[pedidos_df['codigo_cliente'] == codigo_cliente]
-            items = []
-            for _, row in df.iterrows():
-                id_item = str(row['codigo_producto'])
-                quantity = row['cantidad']
-                url = f"https://api.alegra.com/api/v1/items/{id_item}"
-                headers = {
-                    "accept": "application/json",
-                    "authorization": "Basic your_api_key"
-                }
-                response_item = requests.get(url, headers=headers).json()
-                item = {
-                    "id": int(response_item['id']),
-                    "quantity": int(row['cantidad']),
-                    "price": int(response_item['price'][0]['price'])
-                }
-                items.append(item)
-
-            payload = {
-                "status": "open",
-                "paymentForm": "CASH",
-                "paymentMethod": "CASH",
-                "client": {"id": int(df['codigo_cliente'].iloc[0])},
-                "date": df['fecha_creacion'].iloc[0],
-                "dueDate": df['fecha_creacion'].iloc[0],
-                "items": items,
-                "anotation": df['nombre_establecimiento'].iloc[0] + ', ' + df['numero_orden'].iloc[0]
-            }
-            headers = {
-                "accept": "application/json",
-                "content-type": "application/json",
-                "authorization": "Basic your_api_key"
-            }
-            response = requests.post("https://api.alegra.com/api/v1/invoices", json=payload, headers=headers)
-            response_json = response.json()
-            
-            id = int(response_json['id'])
-
-            url = "https://api.alegra.com/api/v1/invoices/stamp"
-            payload = { "ids": [id] }
-            headers = {
-                        "accept": "application/json",
-                        "authorization": "Basic your_api_key",
-                        "content-type": "application/json"
-                    }
-            
-            response = requests.post(url, json=payload, headers=headers)
-            response_invoice = response.json()
-            print(response_invoice)
+        invoces_send_alegra(df=df)
 
         return "facturas electrónicas de venta han sido emitidas exitosamente"
     except:
---
+++ invoice_send.py
@@ -0,0 +1,100 @@
+import requests
+import pandas as pd
+from datetime import datetime
+from customer_update import visualizar_clientes
+from items_update import visualizar_productos
+
+
+def invoces_send_alegra(df):
+    try:
+        clientes = visualizar_clientes().drop_duplicates()
+        nuevos_nombres = {'ID': 'codigo_cliente', 'Name': 'nombre_cliente'}
+        clientes = clientes.rename(columns=nuevos_nombres)
+        productos = visualizar_productos().drop_duplicates()
+        nuevos_nombres = {'ID': 'codigo_producto', 'Name': 'nombre', 'UnitCost':'precio_unitario'}
+        productos = productos.rename(columns=nuevos_nombres)
+        pedidos_df = df.copy()
+
+        cod_cliente = []
+        for razon_social in list(pedidos_df['razon_social']):
+            resultado_loc = clientes.loc[clientes['nombre_cliente'] == razon_social]
+            cod_cliente.append(resultado_loc['codigo_cliente'].iloc[0])
+
+        cod_producto = []
+        for nombre_producto in list(pedidos_df['nombre_producto']):
+            resultado_loc = productos.loc[productos['nombre'] == nombre_producto]
+            cod_producto.append(resultado_loc['codigo_producto'].iloc[0])
+
+        unit_price = []
+        for nombre_producto in list(pedidos_df['nombre_producto']):
+            resultado_loc = productos.loc[productos['nombre'] == nombre_producto]
+            unit_price.append(resultado_loc['precio_unitario'].iloc[0])
+
+        df_codigos = {
+                        'codigo_cliente' :cod_cliente,
+                        'codigo_producto' : cod_producto,
+                        'precio_unitario' :unit_price,
+                        'fecha_creacion'  : datetime.now().strftime('%Y-%m-%d')
+                    }
+        
+        df_codigos = pd.DataFrame(df_codigos)
+        pedidos_df = pd.concat([pedidos_df, df_codigos], axis=1)
+
+        # Eliminar filas con valores NaN
+        pedidos_df.dropna(inplace=True)
+
+        # Conversión de tipos de datos
+        pedidos_df = pedidos_df.dropna().astype({'codigo_cliente': int, 'codigo_producto': int, 'cantidad': int, 'precio_unitario': int})
+
+        for codigo_cliente in pedidos_df['codigo_cliente'].unique():
+            df = pedidos_df[pedidos_df['codigo_cliente'] == codigo_cliente]
+            items = []
+            for _, row in df.iterrows():
+                id_item = str(row['codigo_producto'])
+                quantity = row['cantidad']
+                url = f"https://api.alegra.com/api/v1/items/{id_item}"
+                headers = {
+                    "accept": "application/json",
+                    "authorization": "Basic your_api_key"
+                }
+                response_item = requests.get(url, headers=headers).json()
+                item = {
+                    "id": int(response_item['id']),
+                    "quantity": int(row['cantidad']),
+                    "price": int(response_item['price'][0]['price'])
+                }
+                items.append(item)
+
+            payload = {
+                "status": "open",
+                "paymentForm": "CASH",
+                "paymentMethod": "CASH",
+                "client": {"id": int(df['codigo_cliente'].iloc[0])},
+                "date": df['fecha_creacion'].iloc[0],
+                "dueDate": df['fecha_creacion'].iloc[0],
+                "items": items,
+                "anotation": df['nombre_establecimiento'].iloc[0] + ', ' + df['numero_orden'].iloc[0]
+            }
+            headers = {
+                "accept": "application/json",
+                "content-type": "application/json",
+                "authorization": "Basic bWVyY2Fkb2Fncmljb2xhZGVsYXNpZXJyYTNAZ21haWwuY29tOjI0MTFmYTQ3NzUyMjRjYTkyNWNk"
+            }
+            response = requests.post("https://api.alegra.com/api/v1/invoices", json=payload, headers=headers)
+            response_json = response.json()
+            
+            id = int(response_json['id'])
+
+            url = "https://api.alegra.com/api/v1/invoices/stamp"
+            payload = { "ids": [id] }
+            headers = {
+                        "accept": "application/json",
+                        "authorization": "Basic bWVyY2Fkb2Fncmljb2xhZGVsYXNpZXJyYTNAZ21haWwuY29tOjI0MTFmYTQ3NzUyMjRjYTkyNWNk",
+                        "content-type": "application/json"
+                    }
+            
+            response = requests.post(url, json=payload, headers=headers)
+            response_invoice = response.json()
+            print(response_invoice)
+    except:
+        return 'Error al procesar facturas'
